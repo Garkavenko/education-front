f80ddb13075f26b3c9fef6b92c5f1010b1a37d0d
Garkavenko <a.garkavenko@debex.ru>
2020-07-12 20:26:33 +0300

    commit

--- src/utils/index.js
+++
@@ -1,9 +0,0 @@
-import {API_URL} from "../constants";
-
-export function getUrl(url) {
-    const safeUrl = url || '';
-    if (safeUrl.includes('http')) {
-        return url;
-    }
-    return `${API_URL}${url}`;
-}
